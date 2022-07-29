84ddb71bfd86241e51b7263444d5a3d121775402
raphaelpassos <raphael_magalhaes@live.com>
2022-07-28 23:57:40 -0300

    destinationSearch(Part1)

--- src/componentes/TelaPasseios/index.js
+++ src/componentes/TelaPasseios/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { StyleSheet, View, Button, Image, Text } from 'react-native';
 import { SafeAreaView, ScrollView, StatusBar } from 'react-native-web';
 import SearchResultsScreen from '../../screens/SearchResults';
-import Passeios from '../Passeios';
+import DestinationSearchScreen from '../../screens/DestinationSearch';
 
 export default function TelaPasseios() {
   return (
     <ScrollView>
 <StatusBar barStyle="dark-content"/>
     <SafeAreaView>
-       <SearchResultsScreen/>
+       {/* <SearchResultsScreen/> */}
+       <DestinationSearchScreen/>
     </SafeAreaView>
 </ScrollView>
   );
---
+++ src/screens/DestinationSearch/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 20,
+    },
+    textInput: {
+        fontSize: 20,
+    }
+
+});
+export default styles;
